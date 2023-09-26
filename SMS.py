34cc30a25ff5cc28c0a6125803cebb232379ff44
pavluchkina <114096673+PythonZem@users.noreply.github.com>
2023-09-26 23:49:15 +0300

    Create SMS.py

---
+++ SMS.py
@@ -0,0 +1,19 @@
+import os
+from twilio.rest import Client
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
+
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+client = Client(account_sid, auth_token)
+
+
+def send_sms(text: str):
+    message = client.messages \
+        .create
+            body=text,
+            from_='+12568294379',
+            to='+380503962864'
+        )
+
+
